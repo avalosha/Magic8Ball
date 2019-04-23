923ac7aca453bd5425eda47c24b8c4e09151e5fe
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2019-04-23 17:21:04 -0500

    Add icons and images. Update main storyboard.

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Álvaro Ávalos Hernández on 4/23/19.
+//  Copyright © 2019 Álvaro Ávalos Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
+
