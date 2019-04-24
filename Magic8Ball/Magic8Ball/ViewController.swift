08cc62fe402b540fdf05963b80dc42e491d4e846
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2019-04-23 19:31:59 -0500

    Add functionality to the magic ball.

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -12,13 +12,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    var ballArray = ["ball1","ball2","ball3","ball4","ball5"]
+    var randomBallNumber : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func askButtonPressed(_ sender: Any) {
-        
+        newBallImage()
+    }
+    
+    func newBallImage(){
+        randomBallNumber = Int(arc4random_uniform(5))
+        imageView.image = UIImage.init(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
     }
     
 }
